33ec16faafbb8c4ac0095bf45d8f6ec2b8e5ba5c
mntvsdesign <gpdomiziani@gmail.com>
2026-03-14 22:32:29 +0100

    minor

--- tools/__init__.py
+++ tools/__init__.py
@@ -18,33 +18,6 @@ from .web_tools import (
     tool_definitions as web_tool_definitions,
 )
 
-from .commodity_tool import (
-    commodity_price,
-    CommodityPriceTool,
-    CommodityHistoryTool,
-    GMEPriceTool,
-    get_commodity_price_tool,
-    get_commodity_history_tool,
-    get_gme_price_tool,
-    get_available_commodities,
-    get_commodity_info,
-    get_commodity_tool_definitions,
-    get_tool_metrics,
-    health_check,
-)
-
-from .alpha_api_translation import (
-    CommodityTagsTranslator,
-    TranslationResult,
-    TranslationStatus,
-    AlphaAPIConfig,
-    COMMODITY_TAGS,
-    COMMODITY_TAGS_ALPHA,
-    get_commodity_translator,
-    get_available_tags,
-    get_tag_info,
-    is_alpha_vantage_available,
-)
 
 __all__ = [
     # Web tools
@@ -56,28 +29,4 @@ __all__ = [
     "register_conversation_urls",
     "validate_url_for_fetch",
     "web_tool_definitions",
-    # Commodity tools
-    "commodity_price",
-    "CommodityPriceTool",
-    "CommodityHistoryTool",
-    "GMEPriceTool",
-    "get_commodity_price_tool",
-    "get_commodity_history_tool",
-    "get_gme_price_tool",
-    "get_available_commodities",
-    "get_commodity_info",
-    "get_commodity_tool_definitions",
-    "get_tool_metrics",
-    "health_check",
-    # Alpha API translation layer
-    "CommodityTagsTranslator",
-    "TranslationResult",
-    "TranslationStatus",
-    "AlphaAPIConfig",
-    "COMMODITY_TAGS",
-    "COMMODITY_TAGS_ALPHA",
-    "get_commodity_translator",
-    "get_available_tags",
-    "get_tag_info",
-    "is_alpha_vantage_available",
 ]
